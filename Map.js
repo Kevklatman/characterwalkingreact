e6aa84940e53dc5799c33e6e53bd3586648e3c40
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-17 15:33:24 -0600

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+  return (
+    <div className="map">
+      <img src="NewpalletTown.PNG" alt="Map" className="map-image" />
+    </div>
+  );
+};
+
+export default Map;
